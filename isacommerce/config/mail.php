d9041dd3ce9d3b26c28bb0190110c0ff22cd034f
diyahnf2 <diyah.nf@idesolusiasia.com>
2017-08-03 12:59:23 +0700

    add sendgrid credentials

--- isacommerce/config/mail.php
+++ isacommerce/config/mail.php
@@ -6,6 +6,6 @@ return [
     'port' => 465,
     'from' => array('address' => 'admin@idesolusiasia.com', 'name' => 'IsaCommerce'),
     'encryption' => 'ssl',
-    'username' => '',
-    'password' => '',
+    'username' => 'ismail.saleh',
+    'password' => 'I5m41ling',
 ];
