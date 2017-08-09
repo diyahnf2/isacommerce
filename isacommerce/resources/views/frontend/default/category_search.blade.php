686a435f9741f43f8ce4d9f9102e4993ecf2e077
elishalimanu <30402049+elishalimanu@users.noreply.github.com>
2017-08-09 08:52:41 +0700

    Fix wishlist at search page

--- isacommerce/resources/views/frontend/default/category_search.blade.php
+++ isacommerce/resources/views/frontend/default/category_search.blade.php
@@ -205,7 +205,7 @@
                                         <img class="img-responsive" alt="product" src="{{ url('theme/backend/images/product/', [$p->image])}}" />
                                     </a>
                                     <div class="quick-view">
-                                            <a title="Add to my wishlist" class="heart" href="#"></a>
+                                            <a title="Add to my wishlist" class="heart wishlist_{{$p->id}}" href="#" onclick="wishlist({{$p->id}})" id="{{$p->id}}"></a>
                                             <a title="Add to compare" class="compare" href="#"></a>
                                             <a title="Quick view" class="search" href="#"></a>
                                     </div>
@@ -267,5 +267,6 @@
 @include('frontend.default.partials.footer')
 @include('frontend.default.partials.javascript')
 <script type="text/javascript" src="{{ url('theme/frontend/default/assets/js/filter-product.js')}}"></script>
+<script type="text/javascript" src="{{ url('theme/frontend/default/assets/js/wishlist.js')}}"></script>
 </body>
 </html>
